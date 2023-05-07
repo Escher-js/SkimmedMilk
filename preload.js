67a7160c0fc0681e556b65c48fee62126d883f6c
psmuler <zhmuler@gmail.com>
2023-05-07 21:13:41 +0900

    Auto-commit on 07/05/2023, 21:13:41

--- preload.js
+++ preload.js
@@ -1,5 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const { Diff2Html } = require('diff2html');
+const fs = require('fs');
+const { exec } = require('child_process');
+const gitignoreDefaults = require('./gitignore_defaults');
+
 
 contextBridge.exposeInMainWorld('electronAPI', {
     getDiffHtml: (diff) => {
